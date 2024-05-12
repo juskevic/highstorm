b3c65a8ac05c5d3b4faefe036e6be68589eeba53
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-12 21:29:56 +0200

    Add CSS to disable text selection
    This commit includes adding a block of CSS rules into the application to disable user text selection across various browsers. This implementation is added to enhance user interface interaction and prevent unnecessary text highlighting.

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// Stop ppl from selecting text
+const style = document.createElement('style');
+
+style.innerHTML = `
+* {
+  -webkit-user-select: none;  /* Chrome all / Safari all */
+  -moz-user-select: none;     /* Firefox all */
+  -ms-user-select: none;      /* IE 10+ */
+  user-select: none;          /* Likely future */      
+}`;
+
+document.head.appendChild(style);
