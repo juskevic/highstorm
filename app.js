89d866c97f95425c4c844c95b49d07411af129c5
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-24 16:08:21 +0200

    Add sunrise and sunset information to weather app
    A function for converting Unix timestamp to time format has been added to app.js. New sections for displaying sunrise and sunset data are also added to index.html. This provides users with more comprehensive weather information.

--- app.js
+++ app.js
@@ -32,5 +32,24 @@ function updateUI(data) {
     document.getElementById('location-wind').textContent = `${data.wind.speed} km/h`;
     document.getElementById('location-humidity').textContent = `${data.main.humidity} %`;
     document.getElementById('location-clouds').textContent = `${data.clouds.all} %`
+    document.getElementById('location-sunrise').textContent = unixToTime(data.sys.sunrise);
+    document.getElementById('location-sunset'). textContent = unixToTime(data.sys.sunset)
 }
 fetchWeatherData().catch((error) => console.error('Error:', error));
+
+
+/**
+ * Converts a Unix timestamp to a formatted time string.
+ *
+ * @param {number} unix_timestamp - The Unix timestamp to convert.
+ * @return {string} The formatted time string in 'HH:MM:SS' format.
+ */
+function unixToTime(unix_timestamp) {
+    let date = new Date(unix_timestamp * 1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    let seconds = "0" + date.getSeconds();
+
+    // Will display time in 'HH:MM:SS' format
+    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+}
